2f3947293ec175cfb17876b6671ef5f024c1fdc2
Júlio Alves <julio619alves@gmail.com>
2021-04-16 19:40:38 -0300

    cria funcionario.cpp

---
+++ funcionario.cpp
@@ -0,0 +1,63 @@
+// Universidade Federal de Viçosa - Ciência da Computação
+// Disciplina: INF112 - Programação II
+// Autor: José Júlio Alves Campolina
+// Número de Matrícula: 102022 
+
+#include "funcionario.h"
+
+Funcionario::Funcionario(std::string id, std::string nome, std::string cpf, std::string telefone, std::string endereco, double salario, std::string conta):
+    _id(id), _nome(nome), _cpf(cpf), _telefone(telefone), _endereco(endereco), _salario(salario), _conta(conta) {}
+
+std::string Funcionario::get_id() {
+    return _id;
+}
+
+
+std::string Funcionario::get_nome() {
+    return _nome;
+}
+
+void Funcionario::set_nome(std::string nome) {
+    _nome = nome;
+}
+
+std::string Funcionario::get_cpf() {
+    return _cpf;
+}
+
+void Funcionario::set_cpf(std::string cpf) {
+    _cpf = cpf;
+}
+
+std::string Funcionario::get_telefone() {
+    return _telefone;
+}
+
+void Funcionario::set_telefone(std::string telefone) {
+    _telefone = telefone;
+}
+
+std::string Funcionario::get_endereco() {
+    return _endereco;
+}
+
+void Funcionario::set_endereco(std::string endereco) {
+    _endereco = endereco;
+}
+
+double Funcionario::get_salario() {
+    return _salario;
+}
+
+void Funcionario::set_salario(double salario) {
+    _salario = salario;
+}
+
+std::string Funcionario::get_conta() {
+    return _conta;
+}
+
+void Funcionario::set_conta(std::string conta) {
+    _conta = conta;
+}
+
